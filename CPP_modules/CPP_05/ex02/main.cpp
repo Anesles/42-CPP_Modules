8c21d5e6f2277a6ff0a1b770fdf6a3a4019026e5
brumarti <bbmp2002@gmail.com>
2024-12-18 18:46:43 +0000

    Made more changes

---
+++ CPP_modules/CPP_05/ex02/main.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brumarti <brumarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/10 15:11:38 by brumarti          #+#    #+#             */
+/*   Updated: 2024/05/10 16:26:20 by brumarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main() {
+    // Test Bureaucrat constructors
+    Bureaucrat b1;
+    Bureaucrat b2("John", 42);
+    Bureaucrat b3(b2);
+
+    // Test Bureaucrat assignment operator
+    b1 = b2;
+
+    // Test Bureaucrat methods
+    std::cout << b1.getName() << " has grade " << b1.getGrade() << std::endl;
+    b1.incrementGrade();
+    std::cout << b1.getName() << " has grade " << b1.getGrade() << std::endl;
+    b1.decrementGrade();
+    std::cout << b1.getName() << " has grade " << b1.getGrade() << std::endl;
+
+    // Test Bureaucrat exceptions
+    try {
+        Bureaucrat b4("High", 0);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+    try {
+        Bureaucrat b5("Low", 151);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    // Test Form constructors
+    Form f1;
+    Form f2("Form1", false, 50, 100);
+    Form f3(f2);
+
+    // Test Form assignment operator
+    f1 = f2;
+
+    // Test Form methods
+    std::cout << f1.getName() << " is " << (f1.getSigned() ? "signed" : "not signed") << std::endl;
+    f1.setSigned(true);
+    std::cout << f1.getName() << " is " << (f1.getSigned() ? "signed" : "not signed") << std::endl;
+
+    // Test Form exceptions
+    Bureaucrat b4;
+    try {
+        f1.beSigned(b4);
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    // Test Bureaucrat signing a form
+    b2.signForm(f2);
+
+    return 0;
+}
---
+++ CPP_modules/CPP_05/ex02/srcs/Bureaucrat.cpp
@@ -0,0 +1,115 @@
+#include "Bureaucrat.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+	std::cout << this->_name <<" is here !" << std::endl;
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat & src ) : _name(src._name)
+{
+	*this = src;
+	std::cout << this->_name <<" is here !" << std::endl;
+}
+
+Bureaucrat::Bureaucrat( std::string name, int grade ) : _name(name), _grade(grade)
+{
+	checkGrade(grade);
+	std::cout << this->_name <<" is here !" << std::endl;
+}
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << this->_name <<" is gone..." << std::endl;
+}
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Bureaucrat &				Bureaucrat::operator=( Bureaucrat const & rhs )
+{
+	this->_grade = rhs._grade;
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& stream, Bureaucrat& bur) {
+	stream << bur.getName() << ", bureaucrat grade " << bur.getGrade() << '.' << std::endl;
+	return (stream);
+}
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+void Bureaucrat::checkGrade(int grade)
+{
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	return;
+}
+
+void Bureaucrat::incrementGrade() {
+	int newGrade = (this->getGrade() - 1);
+
+	checkGrade(newGrade);
+	this->_grade = newGrade;
+}
+
+void Bureaucrat::decrementGrade() {
+	int newGrade = (this->getGrade() + 1);
+
+	checkGrade(newGrade);
+	this->_grade = newGrade;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade is too high";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Grade is too low";
+}
+
+void Bureaucrat::signForm(Form & form) {
+	if (form.getSigned() == true) {
+		std::cout << "Formn " << form.getName() << " is already signed." << std::endl;
+	} else {
+		try
+		{
+			form.beSigned(*this);
+			std::cout << this->getName() << " signed form " << form.getName() << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+
+			std::cerr << this->getName() << " couldn't sign form " << form.getName() << " because: " << e.what() << std::endl;
+		}
+		
+	}
+}
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+	std::string Bureaucrat::getName() const {
+		return this->_name;
+	}
+
+	int Bureaucrat::getGrade() const {
+		return this->_grade;
+	}
+
+/* ************************************************************************** */
